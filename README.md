0c83b9085947bbdaa7e82eb50b2bc0878a6dfef4
smartkuk <pipe0502@gmail.com>
2023-04-26 15:25:30 +0900

    update

---
+++ README.md
@@ -0,0 +1,53 @@
+
+```sh
+curl http://localhost:5001
+```
+
+```sh
+curl http://localhost:5001/
+```
+
+```sh
+curl http://localhost:5001/health
+```
+
+```sh
+curl http://localhost:5001/header
+```
+
+```sh
+curl -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"user_id": "1", "user_name": "Trump"}' \
+  http://localhost:5001/users
+```
+
+```sh
+curl -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"user_id": "2", "user_name": "Obama"}' \
+  http://localhost:5001/users
+```
+
+```sh
+curl -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"user_id": "3", "user_name": "Biden"}' \
+  http://localhost:5001/users
+```
+
+```sh
+curl http://localhost:5001/users
+```
+
+```sh
+curl http://localhost:5001/users/2
+```
+
+```sh
+curl http://localhost:5001/users?user_name=Trump
+```
+
+```sh
+curl -X DELETE http://localhost:5001/users/2
+```
--- web/main.py
+++ web/main.py
@@ -1,12 +1,12 @@
-import logging
-from flask import Flask, jsonify, make_response
+from json import JSONEncoder
+import pprint
+from typing import Dict
+from flask import Flask, jsonify,  abort
 from flask import request
 from os import environ
 
-CONTEXT_PATH="/"
-if "CONTEXT_PATH" in environ:
-    context_path = environ["CONTEXT_PATH"]
-    logging.info(f"Using CONTEXT_PATH -> {context_path}")
+app = Flask(__name__)
+
 
 class PrefixMiddleware(object):
 
@@ -15,36 +15,153 @@ class PrefixMiddleware(object):
         self.prefix = prefix
 
     def __call__(self, environ, start_response):
-
         if environ['PATH_INFO'].startswith(self.prefix):
             environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
             environ['SCRIPT_NAME'] = self.prefix
             return self.app(environ, start_response)
-        else:
-            start_response('404', [('Content-Type', 'text/plain')])
-            return ["This url does not belong to the app.".encode()]
 
-app = Flask(__name__)
-app.debug = True
-app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix=CONTEXT_PATH)
+        start_response('404', [('Content-Type', 'text/plain')])
+        return ["This url does not belong to the app.".encode()]
+
+
+class User:
+    def __init__(self, **kwargs):
+        self._user_id: str = None
+        self._user_name: str = None
+
+        if "user_id" in kwargs:
+            self._user_id = kwargs["user_id"]
+        if "user_name" in kwargs:
+            self._user_name = kwargs["user_name"]
+
+    @property
+    def user_id(self) -> str:
+        return self._user_id
+
+    @user_id.setter
+    def user_id(self, value: str):
+        self._user_id = value
+
+    @property
+    def user_name(self) -> str:
+        return self._user_name
+
+    @user_name.setter
+    def user_name(self, value: str):
+        self._user_name = value
+
+    def __str__(self) -> str:
+        return pprint.pformat(self.__dict__)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def serialize(self) -> dict:
+        return {
+            "user_id": self.user_id,
+            "user_name": self.user_name
+        }
+
+
+class SimpleJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, User):
+            return obj.serialize()
+        return super(SimpleJSONEncoder, self).default(obj)
+
+
+app.json_encoder = SimpleJSONEncoder
+
+USERS: Dict[str, User] = {}
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
 @app.route("/health")
 def get_health_check():
     return jsonify({"status": "OK"})
+
 
 @app.route("/header")
 def get_header():
     data = {}
     for key in request.headers.keys():
         data[key] = request.headers.get(key, "")
-    # return make_response(jsonify(request.headers), 200)
     return jsonify(data)
-    
-    
 
+
+@app.route("/users", methods=["POST"])
+def create_user():
+    app.logger.debug(f"request: {request}")
+    if request.is_json is False:
+        abort(400, "Http header application/json is required")
+
+    data = request.get_json()
+    user = User(**data)
+    USERS[user.user_id] = user
+
+    return jsonify(user), 200
+
+
+@app.route("/users", methods=["GET"], defaults={"user_id": None})
+@app.route("/users/<user_id>", methods=["GET"])
+def get_users(user_id: str):
+    if user_id:
+        if user_id in USERS:
+            return jsonify(USERS[user_id]), 200
+        return jsonify(message=f"Not found user by user_id={user_id}"), 404
+
+    if "user_name" in request.args:
+        user_name = request.args.get('user_name')
+        for _, user in USERS.items():
+            if user.user_name == user_name:
+                return jsonify(user), 200
+        return jsonify(message=f"Not found user by user_name={user_name}"), 404
+
+    return jsonify([user for _, user in USERS.items()]), 200
+
+
+@app.route("/users/<user_id>", methods=["DELETE"])
+def remove_user(user_id: str):
+    if user_id in USERS:
+        user = USERS[user_id]
+        del USERS[user_id]
+        return jsonify(user), 200
+
+    return jsonify(message=f"Not found user by user_id={user_id}"), 404
+
+
+def validate_context_path(context_path: str) -> bool:
+
+    if not context_path.startswith("/"):
+        raise ValueError(
+            f"Context path must start with '/' string.(CONTEXT_PATH: {context_path})")
+
+    if len(context_path) != 1 and context_path.endswith("/"):
+        raise ValueError(
+            f"Context path must not end with '/' string.(CONTEXT_PATH: {context_path})")
+
+
+# curl http://localhost:5001/users
+# curl http://localhost:5001/users
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5001', debug=True)
+
+    app.debug = True
+    app.wsgi_app = PrefixMiddleware(app.wsgi_app)
+
+    if "CONTEXT_PATH" in environ:
+
+        validate_context_path(environ['CONTEXT_PATH'])
+
+        app.wsgi_app = PrefixMiddleware(
+            app.wsgi_app, prefix=environ["CONTEXT_PATH"])
+        app.logger.info(
+            f"Configured Env CONTEXT_PATH: {environ['CONTEXT_PATH']}")
+
+    port = environ["PORT"] if "PORT" in environ else 5001
+
+    app.logger.info(f"Expose port number {port}")
+    app.run(host='0.0.0.0', port=port, debug=True)
